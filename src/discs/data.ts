fbd9a47640e116bdc9ce7ab56e32a87e3796c78f
Raimon <raimon.txollo@gmail.com>
2025-03-10 21:43:13 +0100

    Add data and type discs files

---
+++ src/discs/data.ts
@@ -0,0 +1,14 @@
+import { Disc } from "./types.js";
+
+export const discs: Disc[] = [
+  {
+    id: 1,
+    name: "aaa",
+    artist: "aaa",
+    cover: "aaa",
+    format: "aaa",
+    price: 123,
+    originalPrice: 123,
+    isAlbumOfTheMonth: false,
+  },
+];
---
+++ src/discs/types.ts
@@ -0,0 +1,10 @@
+export type Disc = {
+  id: number;
+  name: string;
+  artist: string;
+  cover: string;
+  format: string;
+  price: number;
+  originalPrice?: number;
+  isAlbumOfTheMonth: boolean;
+};
