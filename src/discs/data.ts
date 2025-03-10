1232a3dfcc435318a141205a25e493854fc7ce23
Isabel Sáenz <isasaete@gmail.comn>
2025-03-10 23:33:08 +0100

    Add info to the discs array

--- src/discs/data.ts
+++ src/discs/data.ts
@@ -3,12 +3,145 @@ import { Disc } from "./types.js";
 export const discs: Disc[] = [
   {
     id: 1,
-    name: "aaa",
-    artist: "aaa",
-    cover: "aaa",
-    format: "aaa",
-    price: 123,
-    originalPrice: 123,
+    name: "Bridges to Babylon",
+    artist: "The Rolling Stones",
+    cover: "public/images/covers/rolling-stones-briges-babylon.webp",
+    format: "VINYL",
+    price: 21.99,
+    isAlbumOfTheMonth: false,
+  },
+
+  {
+    id: 2,
+    name: "Sueño su boca",
+    artist: "Raúl",
+    cover: "public/images/covers/raul-sueno-su-boca.webp",
+    format: "VINYL",
+    price: 99.99,
+    originalPrice: 119.99,
+    isAlbumOfTheMonth: false,
+  },
+
+  {
+    id: 3,
+    name: "Temple of the Dog",
+    artist: "Temple of the Dog",
+    cover: "public/images/covers/temple-of-the-dog-temple-of-the-dog.webp",
+    format: "DIGITAL",
+    price: 23.99,
+    isAlbumOfTheMonth: false,
+  },
+
+  {
+    id: 4,
+    name: "Whenever you need somebody",
+    artist: "Rick Astley",
+    cover: "public/images/covers/rick-astley-whenever-you-need-somebody.webp",
+    format: "DIGITAL",
+    price: 269.99,
+    originalPrice: 359.99,
+    isAlbumOfTheMonth: false,
+  },
+
+  {
+    id: 5,
+    name: "Marifé de Triana",
+    artist: "Marifé de Triana",
+    cover: "public/images/covers/marife-de-triana-marife-de-triana.webp",
+    format: "VINYL",
+    price: 219.99,
+    isAlbumOfTheMonth: false,
+  },
+
+  {
+    id: 6,
+    name: "Show me love",
+    artist: "Robin S",
+    cover: "public/images/covers/robin-s-show-me-love.webp",
+    format: "DIGITAL",
+    price: 19.99,
+    isAlbumOfTheMonth: false,
+  },
+
+  {
+    id: 7,
+    name: "Homework",
+    artist: "Daft Punk",
+    cover: "public/images/covers/daft-punk-homework.webp",
+    format: "VINYL",
+    price: 24.59,
+    isAlbumOfTheMonth: false,
+  },
+
+  {
+    id: 8,
+    name: "El Chiringuito",
+    artist: "Georgie Dann",
+    cover: "public/images/covers/george-dann-el-chiringuito.webp",
+    format: "VINYL",
+    price: 1.99,
+    isAlbumOfTheMonth: true,
+  },
+
+  {
+    id: 9,
+    name: "Deep Purple in Rock",
+    artist: "Deep Purple",
+    cover: "public/images/covers/deep-purple-deep-purple-in-rock.webp",
+    format: "VINYL",
+    price: 27.49,
+    isAlbumOfTheMonth: false,
+  },
+
+  {
+    id: 10,
+    name: "Led Zeppelin IV",
+    artist: "Led Zeppelin IV",
+    cover: "public/images/covers/led-zepellin-iv-led-zepellin.webp",
+    format: "VINYL",
+    price: 34.99,
+    isAlbumOfTheMonth: false,
+  },
+
+  {
+    id: 11,
+    name: "Raro",
+    artist: "El Cuarteto de Nos",
+    cover: "public/images/covers/raro-el-cuarteto-de-nos.webp",
+    format: "DIGITAL",
+    price: 19.99,
+    originalPrice: 39.99,
+    isAlbumOfTheMonth: false,
+  },
+
+  {
+    id: 12,
+    name: "De paso / Libertad",
+    artist: "Luis Eduardo Aute",
+    cover: "public/images/covers/aute-de-paso-libertad.webp",
+    format: "DIGITAL",
+    price: 23.99,
+    isAlbumOfTheMonth: false,
+  },
+
+  {
+    id: 13,
+    name: "Agila",
+    artist: "Extremoduro",
+    cover: "public/images/covers/extremoduro-aguila.webp",
+    format: "DIGITAL",
+    price: 59.99,
+    originalPrice: 70.99,
+    isAlbumOfTheMonth: false,
+  },
+
+  {
+    id: 14,
+    name: "Un día en el mundo",
+    artist: "public/images/covers/vetusta-morla-un-dia-en-el-mundo.webp",
+    cover: "URL",
+    format: "DIGITAL",
+    price: 19.99,
     isAlbumOfTheMonth: false,
   },
 ];
