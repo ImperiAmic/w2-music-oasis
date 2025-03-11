566cb8430fd932e7ea716c05437027d2929b4e3f
Isabel Sáenz <isasaete@gmail.comn>
2025-03-11 14:46:47 +0100

    Modify names of type Disc and variables discs, and properties of type

--- src/discs/types.ts
+++ src/discs/types.ts
@@ -1,10 +1,10 @@
-export type Disc = {
+export type Record = {
   id: number;
   name: string;
   artist: string;
-  cover: string;
-  format: string;
+  coverUrl: string;
+  type: "VINYL" | "DIGITAL";
   price: number;
   originalPrice?: number;
-  isAlbumOfTheMonth: boolean;
+  isRecordOfTheMonth: boolean;
 };
